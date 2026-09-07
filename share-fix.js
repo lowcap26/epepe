1832d015fea65be051ed13b5beac485ef1be0ed4
lowcap26 <quantumq2020@icloud.com>
2026-09-06 18:26:03 -0700

    Game share: TG, @evilpepelol, card URL

---
+++ share-fix.js
@@ -0,0 +1,5 @@
+var SITE = "https://evilpepe.lol/?v=card";
+var TG_URL = "https://t.me/evilpepelol";
+function tweetText() {
+  return "$EPEPE burned " + score + " bags. Dead bags don't come back.\n\n" + TG_URL + "\n@evilpepelol\n" + SITE;
+}
