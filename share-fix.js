6bcaf7b99f9896c1c00607a9dadeba0ab416f33d
lowcap26 <quantumq2020@icloud.com>
2026-09-06 18:26:08 -0700

    Override share click with TG + handle + card link

--- share-fix.js
+++ share-fix.js
@@ -3,3 +3,10 @@ var TG_URL = "https://t.me/evilpepelol";
 function tweetText() {
   return "$EPEPE burned " + score + " bags. Dead bags don't come back.\n\n" + TG_URL + "\n@evilpepelol\n" + SITE;
 }
+var shareBtn = document.getElementById("share");
+if (shareBtn) {
+  shareBtn.onclick = function () {
+    var text = tweetText();
+    window.open("https://x.com/intent/tweet?text=" + encodeURIComponent(text) + "&url=" + encodeURIComponent(SITE), "_blank", "noopener");
+  };
+}
